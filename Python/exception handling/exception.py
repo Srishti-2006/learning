e2f1ac50419b339e1a0612e1a988de25a927799c
Srishti-2006 <srishtimaurya151@gmail.com>
2026-09-18 12:32:00 +0530

    a example on xception handling

---
+++ Python/exception handling/exception.py
@@ -0,0 +1,36 @@
+#creating custom exception example 
+#using try-> except -> else -> finally
+class SecurityError(Exception):
+    def __init__(self,message):
+        print(message)
+    def logout(self):
+        print('logout')
+
+class Google:
+
+    def __init__(self,name,email,password,device):
+        self.name=name
+        self.email=email
+        self.password=password
+        self.device=device
+
+    def login(self,email,password,device):
+        if device!= self.device:
+            raise SecurityError('bhai teri toh lag gyi')
+        if email==self.email and password== self.password:
+            print('welcome')
+        else:
+            print('login error')
+
+
+
+obj=Google('nitish','nitish@gmail.com','1234','android')
+
+try:
+    obj.login('nitish@gmail.com','1234','windows')
+except SecurityError as e:
+    e.logout()
+else:
+    print(obj.name)
+finally:
+    print('database connection closed')
